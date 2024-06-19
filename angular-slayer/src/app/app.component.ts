c0fd025ca247aa216478f430c354f008ef502b32
ShmPt <cacahouettetbs@gmail.com>
2024-06-19 11:30:34 +0200

    feat: add all elements to app component

--- angular-slayer/src/app/app.component.ts
+++ angular-slayer/src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PlayerHealthBarComponent } from './player-health-bar/player-health-bar.component';
+import { MonsterHealthBarComponent } from './monster-health-bar/monster-health-bar.component';
+import { PlayerAttackButtonComponent } from './player-attack-button/player-attack-button.component';
+import { PlayerSpecialAttackButtonComponent } from './player-special-attack-button/player-special-attack-button.component';
+import { PlayerHealButtonComponent } from './player-heal-button/player-heal-button.component';
+import { PlayerFleeButtonComponent } from './player-flee-button/player-flee-button.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, PlayerHealthBarComponent, MonsterHealthBarComponent, PlayerAttackButtonComponent, PlayerSpecialAttackButtonComponent, PlayerHealButtonComponent, PlayerFleeButtonComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
