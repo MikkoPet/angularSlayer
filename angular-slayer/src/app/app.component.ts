104a710fff228350c24e35db823b1a51c31f1da7
ShMPt <cacahouettetbs@gmail.com>
2024-06-20 11:42:33 +0200

    feat: display current charges held by player

--- angular-slayer/src/app/app.component.ts
+++ angular-slayer/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { PlayerHealthBarComponent } from './player-health-bar/player-health-bar.component';
 import { MonsterHealthBarComponent } from './monster-health-bar/monster-health-bar.component';
@@ -28,5 +28,6 @@ import { PlayerSpecialHealComponent } from './player-special-heal/player-special
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  gameHandlerService = inject(GameHandlerService);
   title = 'angular-slayer';
 }
